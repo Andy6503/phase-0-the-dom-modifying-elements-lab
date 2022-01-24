c775c563a84b72453ba283a67a5eb3f64e738ecb
Andrew <andrew.kapsa1@gmail.com>
2022-01-24 13:22:51 -0500

    commit tree 840081386205da2daa7aaed424aa7cff6823e9d1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector('main').remove("main#main")
 const newHeader = document.createElement('h1')
 newHeader.setAttribute('id','victory')
-newHeader.textContent = "Robin is the champion"
+newHeader.textContent = "Andrew is the champion"
